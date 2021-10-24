763b8e45cc97ca0779539c53cd6e804d6fa00d82
Elad <eladmanor101@gmail.com>
2021-10-24 20:38:32 +0300

    Added math utilities

--- Okay (Puzzle)/src/Utilities/Math.h
+++ Okay (Puzzle)/src/Utilities/Math.h
@@ -2,8 +2,11 @@
 
 #include "SFML/Graphics.hpp"
 
-float dot(sf::Vector2f v1, sf::Vector2f v2);
+namespace math
+{
+	float dot(sf::Vector2f v1, sf::Vector2f v2);
 
-sf::Vector2f normalize(sf::Vector2f v);
+	sf::Vector2f normalize(sf::Vector2f v);
 
-float length(sf::Vector2f v);
+	float length(sf::Vector2f v);
+}
